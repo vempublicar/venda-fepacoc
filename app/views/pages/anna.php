1bad4a70dd5b7bef1a2dbe90ef6140435d7b65ea
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-01-22 09:17:33 -0300

    1

--- app/views/pages/anna.php
+++ app/views/pages/anna.php
@@ -36,7 +36,7 @@
     <div class="">
         <div class="columns is-vcentered">
             <!-- Texto à esquerda -->
-            <div class="column mt-8 is-6 has-text-left">
+            <div class="column is-6 has-text-left" style="margin-top:200px" >
                 <h3>
                 Impulsione sua <span class="highlight-blue">empresa</span> com uma gestão estratégica!
                 </h3>
